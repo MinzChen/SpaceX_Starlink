dc32d706ecab9f7bb6839866daa5e70d9c26ec7d
MinzChen <benjaminchen0917@gmail.com>
2021-07-26 09:30:59 -0500

    Main SatSetting components added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer className="footer">
-                (c)2020 StarLink Tracker. All Rights Reserved. Website Made by Your name
-            </footer>
-        );
-    }
+  render() {
+    return (
+      <footer className="footer">
+        (c)2020 StarLink Tracker. All Rights Reserved. Website Made by Your name
+      </footer>
+    );
+  }
 }
 
 export default Footer;
