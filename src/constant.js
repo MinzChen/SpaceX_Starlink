46fc62954c2a33df0692d8d54c3c2795b2790f67
MinzChen <benjaminchen0917@gmail.com>
2021-07-27 00:56:49 -0500

    axios api setup

---
+++ src/constant.js
@@ -0,0 +1,7 @@
+export const SAT_BASE_URL = "https://api.n2yo.com/rest/v1/satellite";
+
+export const SAT_API_KEY = "R5MULF-46PWEY-ABU9JA-4IYV";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
