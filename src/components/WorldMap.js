8731993bf1620910c1515c126a4c300a7b5dd92b
MinzChen <benjaminchen0917@gmail.com>
2021-08-14 12:39:19 -0400

    sat tracking enabled

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -4,9 +4,8 @@ import axios from 'axios';
 import { geoKavrayskiy7 } from 'd3-geo-projection';
 import { geoGraticule, geoPath } from 'd3-geo';
 import { select as d3Select } from 'd3-selection';
-import {Spin} from 'antd';
-
 import { WORLD_MAP_URL } from "../constant";
+import { Spin } from 'antd';
 
 const width = 960;
 const height = 600;
@@ -15,7 +14,7 @@ class WorldMap extends Component {
     constructor(){
         super();
         this.state = {
-            map: null
+            map: null,
         }
         this.refMap = React.createRef();
     }
@@ -24,7 +23,6 @@ class WorldMap extends Component {
         axios.get(WORLD_MAP_URL)
             .then(res => {
                 const { data } = res;
-                //console.log(data);
                 const land = feature(data, data.objects.countries).features;
                 this.generateMap(land);
             })
@@ -38,6 +36,7 @@ class WorldMap extends Component {
             .precision(.1);
 
         const graticule = geoGraticule();
+        console.log(graticule());
 
         const canvas = d3Select(this.refMap.current)
             .attr("width", width)
@@ -58,35 +57,38 @@ class WorldMap extends Component {
             path(ele);
             context.fill();
             context.stroke();
-
-            // draw the graticule
-            context.strokeStyle = 'rgba(220, 220, 220, 0.1)';
-            context.beginPath();
-            path(graticule());
-            context.lineWidth = 0.1;
-            context.stroke();
-
-            // draw the graticule outline
-            context.beginPath();
-            context.lineWidth = 0.5;
-            path(graticule.outline());
-            context.stroke();
         })
+
+        for (let temp = 1; temp < 15; temp++) {
+          // draw the graticule
+          context.strokeStyle = 'rgba(220, 220, 220, 0.5)';
+          context.beginPath();
+          path(graticule());
+          context.lineWidth = 0.5;
+          context.stroke();
+
+
+          // draw the graticule outline
+          context.beginPath();
+          context.lineWidth = 0.7;
+          path(graticule.outline());
+          context.stroke();
+        }
     }
 
     render() {
         return (
             <div className="map-box">
                 <canvas className="map" ref={this.refMap} />
+                <canvas className="track" ref={this.props.refTrack}/>
+                <div className="hint"></div>
                 {
-                    (this.props.loading) ?
+                    this.props.loading ?
                     <Spin tip="Loading..." /> : <></>
                 }
-
             </div>
         );
     }
 }
 
 export default WorldMap;
-
